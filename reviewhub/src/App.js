016d752a1ad894ec09a6b686d78a7833407443b8
Walter Jacquette <wjacquet@purdue.edu>
2018-12-19 17:22:28 -0500

    yummy

--- reviewhub/src/App.js
+++ reviewhub/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          Kyle Sucks
+          Yummy food goes here
         </header>
       </div>
     );
