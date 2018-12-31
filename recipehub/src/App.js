fa38235f974092b67bc9f7ea37cd86adb3fcab69
Walter Jacquette <wjacquet@purdue.edu>
2018-12-31 15:29:21 -0500

    Router Dom

--- recipehub/src/App.js
+++ recipehub/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import {Route, Switch, Redirect} from 'react-router-dom'
-
 class App extends Component {
   render() {
     return (
